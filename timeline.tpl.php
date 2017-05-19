f5badc38bbc0b7481614e7e39cd2a7185db2a0e0
Michael Daul <michael.daul@duke.edu>
2017-05-19 13:55:35 -0400

    trying to use protocol independent

--- timeline.tpl.php
+++ timeline.tpl.php
@@ -12,40 +12,40 @@ if (strpos($_SERVER['REQUEST_URI'], '/corporations') !== false ){
 <div class="timeline-wrapper">
 
 <!-- Bio Text --->
-	
+
 	<div class="abstractBio">
 		<div id="personPortrait">
-		
+
 			<?php echo '<a class="colorbox cboxElement" rel="colorbox" href="' . $dulmaster_headshot . '">'; ?>
-			
+
 				<?php echo '<img class="portrait subtleBorder smallShadow" property="schema:image" src="' . $dulmaster_headshot . '" alt="' . $dulmaster_name . '">'; ?>
-		
+
 			</a>
 		</div>
-	
+
 		<h1 class="splashTitle" property="schema:name"><?php echo $dulmaster_name ?></h1>
 
 		<div class="personAbout" property="schema:description">
-	
+
 			<?php echo $dulmaster_abstract ?>
-	
+
 		</div>
 
 		<div class="clear"></div>
 
 
 	</div>
-	
-	
+
+
 
 
 
 <!-- Navigation Links -->
-	
+
 	<ul class="mainLinks">
 		<li><a href="#timeline-embed" onclick="_gaq.push(['_trackEvent', 'eacNav','timelineLink']);">Timeline</a></li>
 		<li><a href="#collections" onclick="_gaq.push(['_trackEvent', 'eacNav','collectionsLink']);">Collections</a></li>
-		
+
 		<?php
 		if ($isCorp == true) {
 			echo '<li><a href="#history" onclick="_gaq.push([\'_trackEvent\', \'eacNav\',\'biogHistLink\']);">Administrative History</a></li>';
@@ -55,9 +55,9 @@ if (strpos($_SERVER['REQUEST_URI'], '/corporations') !== false ){
 		}
 		?>
 
-		
+
  	</ul>
- 	
+
  	<div class="clear"></div>
 
 
@@ -65,266 +65,266 @@ if (strpos($_SERVER['REQUEST_URI'], '/corporations') !== false ){
 
 
 <!--- Timeline Embed --->
-	
+
 	<div class="iframe-timeline" id="timeline-embed">
-	
-		<?php echo '<iframe src="http://embed.verite.co/timeline/?source=' . $timeline_key . '&amp;font=Georgia-Helvetica&amp;maptype=toner&amp;lang=en&amp;height=450" width="100%" height="450" frameborder="0"></iframe>'; ?>
-	
+
+		<?php echo '<iframe src="//embed.verite.co/timeline/?source=' . $timeline_key . '&amp;font=Georgia-Helvetica&amp;maptype=toner&amp;lang=en&amp;height=450" width="100%" height="450" frameborder="0"></iframe>'; ?>
+
 	</div>
-	
-	
+
+
 
 
 	<!-- Entries: Column 1 -->
-	
+
 	<div class="grid-8 alpha">
 
-		<?php 
-		
+		<?php
+
 		if ($isCorp == true) {
 			echo '<h2 id="history" class="bioTitle">Administrative History</h2>';
 		}
 		else {
 			echo '<h2 id="history" class="bioTitle">Biographical History</h2>';
 		}
-	
+
 
 		foreach ($data_timeline['feed']['entry'] as $item) {
-	
-	
+
+
 			// Normal entries
 			if ($item['gsx$startdate']['$t'] != "") {
-		
+
 				$thumbCount = 0;
-		
+
 				$cbCount += 1;
-		
+
 				echo '<div class="item-wrapper">';
-		
+
 					echo '<div class="summary">';
-			
+
 						// Dates
 						echo '<div class="dates">';
 
 							// account for year only
 							if (strlen($item['gsx$startdate']['$t']) == 4) {
-								
+
 								echo $item['gsx$startdate']['$t'];
-								
+
 							}
-							
+
 							else {
-							
+
 								echo date('Y M d', strtotime($item['gsx$startdate']['$t']));
-								
+
 							}
-							
+
 							// if there's an end date
 							if ($item['gsx$enddate']['$t'] != "") {
-								
+
 								// account for year only
 								if (strlen($item['gsx$startdate']['$t']) == 4) {
-								
+
 									echo ' &ndash; ' . $item['gsx$enddate']['$t'];
-								
+
 								}
-				
-								else {	
-									
+
+								else {
+
 									echo ' &ndash; ' . date('Y M d', strtotime($item['gsx$enddate']['$t']));
-									
+
 								}
-				
+
 							}
-				
+
 						echo '</div>';
-			
+
 						// Headline
 						echo '<div class="headline">';
-			
+
 							echo $item['gsx$headline']['$t'];
-			
+
 						echo '</div>';
-			
+
 					echo '</div>';
-		
+
 					echo '<div class="details">';
-						
-						
+
+
 						// Thumbnail (if it's empty, display empty div)
-						
+
 						if ($item['gsx$mediathumbnail']['$t'] != "") {
-							
+
 							echo '<div class="timeline-thumbnail">';
-			
+
 								echo '<a class="colorbox cboxElement" data-group="colorboxgroup' . $cbCount . '" title="' . $item['gsx$mediacaption']['$t'] . '" href="' . $item['gsx$media']['$t'] . '">';
-					
+
 								echo '<img src="' . $item['gsx$mediathumbnail']['$t'] . '" alt="' . $item['gsx$mediacaption']['$t'] . '" >';
 								//echo '<span class="credit">' . $item['gsx$mediacredit']['$t'] . '</span>';
-					
+
 								echo '</a>';
-			
+
 							echo '</div>';
-							
+
 							}
-							
+
 							else {
-								
+
 								echo '<div class="timeline-thumbnail">&nbsp;</div>';
-								
+
 							}
-							
-							
-			
+
+
+
 						// Text Description
 						echo '<div class="description">';
 							echo '<p>' . $item['gsx$text']['$t'] . '</p>';
 						echo '</div>';
-		
+
 					echo '</div>';
-		
+
 				echo '</div>';
-	
+
 				echo '<br clear="all" />';
-		
+
 			}
-	
+
 			// Extra images
 			else {
-		
+
 				$thumbCount += 1;
-		
+
 				// Thumbnail
 				echo '<div class="clipwrapper">';
 					echo '<div class="clip">';
-				
+
 						echo '<a class="colorbox cboxElement" data-group="colorboxgroup' . $cbCount . '" title="' . $item['gsx$mediacaption']['$t'] . '" href="' . $item['gsx$media']['$t'] . '">';
-					
+
 						echo '<img src="' . $item['gsx$mediathumbnail']['$t'] . '" alt="' . $item['gsx$mediacaption']['$t'] . '" >';
-				
+
 						echo '</a>';
-				
+
 					echo '</div>';
 				echo '</div>';
-		
+
 				if ($thumbCount % 4 == 0) {
-		
+
 					echo '<br clear="all" />';
-				
+
 				}
-		
+
 			}
-	
+
 		}
-		
+
 		?>
-		
+
 	</div>
-	
-	
+
+
 	<!-- RELATIONS: Column 2 -->
-	
+
 	<div class="grid-4 omega" id="sidebar">
-		
+
 		<div id="accordion" style="max-height: 600px;">
-			
+
 			<!-- Collections Accordian -->
-			
+
 			<?php if(isset($data_associatedcollections['feed']['entry'][0]['gsx$eacurl']['$t'])) { ?>
-			
-			
+
+
 			<h3 id="collections"><a href="#">Associated Collections</a></h3>
 			<div>
 				<ul id="collections_list">
-			
+
 				<?php
-			
+
 				foreach ($data_associatedcollections['feed']['entry'] as $item) {
-				
+
 					echo '<li>';
-						
+
 						echo '<a href="' . $item['gsx$eacurl']['$t'] . '" class="linkFindingAid">' . $item['gsx$eactitle']['$t'] . '</a><br>';
-							
+
 						echo '<span class="collectionDescription">' . $item['gsx$eacdescription']['$t'] . '</span>';
-						
+
 					echo '</li>';
 
 
 				}
 
-	
+
 				?>
-			
-								
+
+
 				</ul>
     		</div>
-    		
+
     		<?php } ?>
-    		
+
     		<!-- People Accordian -->
-    		
+
     		<?php if(isset($data_people['feed']['entry'][0]['gsx$eacurl']['$t'])) { ?>
-    		
+
 			<h3 id="people"><a href="#">People</a></h3>
 			<div>
-				
+
 				<ul id="people_list">
-				    
+
 				<?php
-			
+
 				foreach ($data_people['feed']['entry'] as $item) {
-				
+
 					echo '<li>';
-						
+
 						echo '<a href="' . $item['gsx$eacurl']['$t'] . '" class="linkFindingAid">' . $item['gsx$eactitle']['$t'] . '</a> ' . $item['gsx$eacrelation']['$t'];
-						
+
 					echo '</li>';
 
 
 				}
 
-	
+
 				?>
-				    
-				    		
+
+
 				</ul>
     		</div>
-    		
+
     		<?php } ?>
-            
-    		
+
+
     		<!-- Corporate Bodies Accordian -->
-    		
+
     		<?php if (isset($data_corporatebodies['feed']['entry'][0]['gsx$eacurl']['$t'])) { ?>
-    		
+
 			<h3 id="corporate_bodies"><a href="#">Corporate Bodies</a></h3>
 			<div>
 				<ul id="corporate_bodies_list">
-				
+
 				<?php
-			
+
 				foreach ($data_corporatebodies['feed']['entry'] as $item) {
-				
+
 					echo '<li>';
-						
+
 						echo '<a href="' . $item['gsx$eacurl']['$t'] . '" class="linkFindingAid">' . $item['gsx$eactitle']['$t'] . '</a> ' . $item['gsx$eacrelation']['$t'];
-						
+
 					echo '</li>';
 
 
 				}
 
-	
+
 				?>
-					
+
 				</ul>
     		</div>
-    		
+
     		<?php } ?>
-            
-		
+
+
 		</div>
-		
+
 	</div>
-	
+
 </div>
--- timelinelist.tpl.php
+++ timelinelist.tpl.php
@@ -6,53 +6,53 @@ $result = db_query('SELECT url_slug, dulmaster_num, dulmaster_key FROM {timeline
 
 
 // set timeout length
-$ctx = stream_context_create(array( 
-	'http' => array( 
-		'timeout' => 5 
-		) 
-	) 
-); 
+$ctx = stream_context_create(array(
+	'http' => array(
+		'timeout' => 5
+		)
+	)
+);
 
 
 ?>
 
 <div class="timeline-wrapper">
-	
+
 	<?php
-	
+
 	foreach ($result as $record) {
-		
+
 		$url_slug = $record->url_slug;
 		$dulmaster_num = $record->dulmaster_num;
 		$dulmaster_key = $record->dulmaster_key;
-		
+
 		// Pull in Master Google Spreadsheet as JSON
-		$url_dulmaster = "http://spreadsheets.google.com/feeds/list/" . $dulmaster_key . "/od6/public/values?alt=json&amp;callback=displayContent";
+		$url_dulmaster = "//spreadsheets.google.com/feeds/list/" . $dulmaster_key . "/od6/public/values?alt=json&amp;callback=displayContent";
 		$json_dulmaster = file_get_contents($url_dulmaster, 0, $ctx);
 		$data_dulmaster = json_decode($json_dulmaster, TRUE);
-		
+
 		$dulmaster_name = $data_dulmaster['feed']['entry'][$dulmaster_num]['gsx$entityname']['$t'];
 		$dulmaster_headshot = 'http://library.duke.edu/digitalcollections/media/jpg/' . $data_dulmaster['feed']['entry'][$dulmaster_num]['gsx$slug']['$t'] . '/med/' . $data_dulmaster['feed']['entry'][$dulmaster_num]['gsx$portraitimage']['$t'];
 		$dulmaster_abstract = $data_dulmaster['feed']['entry'][$dulmaster_num]['gsx$abstract']['$t'];
-		
+
 		$timeline_url = $GLOBALS['base_path'] . $root_path .'/' . $url_type . '/' . $url_slug;
-		
-		
+
+
 		echo '<div class="list-item">';
-		
+
 		echo '<div class="thumbnail"><a href="' . $timeline_url . '"><img src="' . $dulmaster_headshot . '"></a></div>';
-		
+
 		echo '<h2><a href="' . $timeline_url . '">' . $dulmaster_name . '</a></h2>';
 
 		echo $dulmaster_abstract;
-		
+
 		echo '</div>';
-		
-		
+
+
 	}
-	
-	
+
+
 	?>
-	
-	
+
+
 </div>
